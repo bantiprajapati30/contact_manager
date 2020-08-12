eca60e0e2e7171537b59c400c34cc59996f37dc3
bantiprajapati30 <62307939+bantiprajapati30@users.noreply.github.com>
2020-08-12 15:28:34 +0530

    Updates

---
+++ precache-manifest.3808f14649109956b7f14aa525bb8a4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be524a85fc10f396a4f2003859d3698f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d475e2e7c45dd929eff7",
+    "url": "/contactmanager/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "2a781bb2f9ce81cec14e",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d475e2e7c45dd929eff7",
+    "url": "/contactmanager/static/js/2.aa83cf62.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.aa83cf62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a781bb2f9ce81cec14e",
+    "url": "/contactmanager/static/js/main.adadacfd.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
