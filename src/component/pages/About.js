dcbb1a220f0640aa66e1b1510fd92c432e6af831
bantiprajapati30 <62307939+bantiprajapati30@users.noreply.github.com>
2020-08-26 09:25:27 +0530

    before ejecting

--- src/component/pages/About.js
+++ src/component/pages/About.js
@@ -4,7 +4,9 @@ export default function About() {
     return (
         <div>
             <h1 className="display-6">About Contact manager</h1>
+            <p className="lead">welcome to my dear friends</p>
             <p className="lead">Simple app to manage contact</p>
+            <p className="lead">this is made by Banti Prajapati </p>
             <p className="text-secondary">Version 1.0.0</p>
         </div>
     )
